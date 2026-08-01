f6394c42af8e7f3c148deced71f31a20292f6187
TheDentalBarns <david@thedentalbarns.com>
2026-08-01 21:14:27 +0100

    Add TDB Swiper module entry point

---
+++ tools/custom-swiper/src/tdb-swiper-8.4.7.js
@@ -0,0 +1,19 @@
+import Swiper, {
+  A11y,
+  Autoplay,
+  Keyboard,
+  Navigation,
+  Pagination,
+  Parallax,
+} from 'swiper';
+
+Swiper.use([
+  A11y,
+  Autoplay,
+  Keyboard,
+  Navigation,
+  Pagination,
+  Parallax,
+]);
+
+window.Swiper = Swiper;
